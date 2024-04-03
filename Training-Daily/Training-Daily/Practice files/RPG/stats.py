db2681309e5a99aa3d8ee97fa72366c24d190bb2
Bharat-Pathrabe <pbharat@trellissoft.ai>
2024-04-03 16:48:40 +0530

    Daily 03-04-24

--- Training-Daily/Training-Daily/Practice files/RPG/stats.py
+++
@@ -1,8 +0,0 @@
-class Stats:
-   @staticmethod
-   def display_stats(player):
-       print(f"Name: {player.get_name()}")
-       print(f"Health: {player.get_health()}")
-       print(f"Attack: {player.get_attack()}")
-       print(f"Defense: {player.get_defense()}")
-       print(f"Gold: {player.get_gold()}")
